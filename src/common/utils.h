f53f4b43a6d3a322384c1f1e18f6c31076c68d70
Julien Adam <julien.adam-dev@sfr.fr>
2014-12-07 22:39:37 +0100

    Adding bases like main and commons

---
+++ src/common/utils.h
@@ -0,0 +1,33 @@
+/***************************************************************************/
+/*                                                                         */
+/* This file is part of Sublime Code.                                      */
+/*                                                                         */
+/* Sublime Code is free software: you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation, either version 3 of the License, or       */
+/* (at your option) any later version.                                     */
+/*                                                                         */
+/* Sublime Code is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with Sublime Code.  If not, see <http://www.gnu.org/licenses/>.   */
+/*                                                                         */
+/***************************************************************************/
+
+#ifndef COMMON_UTILS_H
+#define COMMON_UTILS_H
+
+/******* COMMON *******/
+#include<cstdlib>
+#include<unistd.h>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+
+/**** MACROS *****/
+#define scDebug(u) {std::cout << u << " (" << __FILE__<<":"<<__LINE__<<")"<< std::endl; } while(0)
+
+#endif
