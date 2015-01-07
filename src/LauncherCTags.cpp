0378a8baa7b9871a048076de5cdf5cbecb77c417
Julien Adam <julien.adam-dev@sfr.fr>
2015-01-07 14:00:00 +0100

    Adding LauncherCTags in SublimeCode

---
+++ src/LauncherCTags.cpp
@@ -0,0 +1,67 @@
+/***************************************************************************/
+/*                                                                         */
+/* This file is part of Sublime Code.                                      */
+/*                                                                         */
+/* Sublime Code is free software: you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation, either version 3 of the License, or       */
+/* (at your option) any later version.                                     */
+/*                                                                         */
+/* Sublime Code is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with Sublime Code.  If not, see <http://www.gnu.org/licenses/>.   */
+/*                                                                         */
+/***************************************************************************/
+
+
+#include "LauncherCTags.h"
+using namespace std;
+
+const std::string LauncherCTags::DEF_EXEC = "ctags";
+const std::string LauncherCTags::GLOBAL_OPTIONS = "-Rn --c-kinds=+cdefgmnstuv";
+
+LauncherCTags::LauncherCTags (Configuration *config, std::vector<std::string>* listPaths, string exe, string opts ) {
+	this->pathExecutable = exe;
+	this->listPaths = listPaths;
+	this->config = config;
+	if(opts != GLOBAL_OPTIONS)
+		this->options = opts;
+
+	if(listPaths == NULL)
+		this->listPaths = new vector<string>;
+}
+
+void LauncherCTags::generateTagsFile() {
+	std::string command = pathExecutable + " " + options + " " + GLOBAL_OPTIONS;
+	std::string path = " -f " + config->getDestDir() + "/tags " ;
+	std::string list = "";
+
+	for(vector<string>::iterator it = listPaths->begin(); it != listPaths->end(); it++)
+		list += (*it) + " ";
+
+	command += path + list;
+
+	system(command.c_str());
+}
+
+bool LauncherCTags::addPathToAnalyze ( string path ) {
+	listPaths->push_back(path);
+}
+
+void LauncherCTags::display() const {
+	cout << "LAUNCHER CTAGS ------>" << endl
+	<< "Executable : " << pathExecutable << endl
+	<< "Extra opts : " << options << endl;
+	if(!listPaths->empty())
+		cout << "Paths to analyze :" << endl;
+		for(vector<string>::iterator it = listPaths->begin(); it != listPaths->end(); it++)
+			cout << "\t- " << *it <<endl;
+	cout << "--------------------->" << endl;
+
+}
+
+
---
+++ src/LauncherCTags.h
@@ -0,0 +1,44 @@
+/***************************************************************************/
+/*                                                                         */
+/* This file is part of Sublime Code.                                      */
+/*                                                                         */
+/* Sublime Code is free software: you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation, either version 3 of the License, or       */
+/* (at your option) any later version.                                     */
+/*                                                                         */
+/* Sublime Code is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with Sublime Code.  If not, see <http://www.gnu.org/licenses/>.   */
+/*                                                                         */
+/***************************************************************************/
+
+#ifndef LAUNCHERCTAGS_H
+#define LAUNCHERCTAGS_H
+
+#include "Launcher.h"
+#include "Configuration.h"
+class LauncherCTags
+{
+private:
+	std::string pathExecutable;
+	std::string options;
+	std::vector<std::string>* listPaths;
+	std::string filePath;
+	Configuration* config;
+
+	static const std::string DEF_EXEC;
+	static const std::string GLOBAL_OPTIONS;
+
+public:
+	LauncherCTags(Configuration* config, std::vector<std::string>* listPaths = NULL, std::string exe = "ctags", std::string opts = GLOBAL_OPTIONS);
+	void generateTagsFile();
+	bool addPathToAnalyze(std::string filename);
+	void display() const;
+};
+
+#endif // LAUNCHERCTAGS_H
